a09ea38717f4bb531a10abc0936d288130299496
Pastafredda <justspotify09@gmail.com>
2023-06-02 17:01:37 +0200

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,122 @@
 <script>
 export default {
-    components: {
-        name: "Header"
+    name: "Header",
+    data() {
+        return {
+            links: [
+                {
+                    text: "CHARACTERS",
+                    url: "#",
+                    current: false
+                },
+
+                {
+                    text: "COMICS",
+                    url: "#",
+                    current: true
+                },
+
+                {
+                    text: "MOVIES",
+                    url: "#",
+                    current: false
+                },
+
+                {
+                    text: "TV",
+                    url: "#",
+                    current: false
+                },
+
+                {
+                    text: "GAMES",
+                    url: "#",
+                    current: false
+                },
+
+                {
+                    text: "COLLECTIBLES",
+                    url: "#",
+                    current: false
+                },
+
+                {
+                    text: "VIDEOS",
+                    url: "#",
+                    current: false
+                },
+
+                {
+                    text: "FANS",
+                    url: "#",
+                    current: false
+                },
+
+                {
+                    text: "NEWS",
+                    url: "#",
+                    current: false
+                },
+
+                {
+                    text: "SHOP",
+                    url: "#",
+                    current: false
+                },
+
+            ]
+        }
     }
+
 }
 </script>
 
-<template></template>
+<template>
+    <header>
+        <div class="container">
+            <div class="container-header">
+                <div class="logo">
+                    <a href="/">
+                        <img src="../assets/img/dc-logo.png" alt="">
+                    </a>
+                </div>
+                <div>
+                    <ul class="nav-menu">
+                        <li v-for="link in links"><a :href="link.url" :class="link.current ? 'active' : 'black'">{{
+                            link.text }}</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
 
-<style></style>
+<style lang="scss" scoped>
+@use '../styles/mixins.scss' as *;
+
+.container-header {
+    @include display-flex;
+    padding: 20px;
+
+    .logo {
+        width: 64px;
+    }
+
+    .nav-menu {
+        display: flex;
+        gap: 16px;
+
+        li {
+            list-style-type: none;
+            font-size: 12px;
+
+        }
+
+        a {
+            text-decoration: none;
+            font-weight: 500;
+
+        }
+    }
+}
+</style>
