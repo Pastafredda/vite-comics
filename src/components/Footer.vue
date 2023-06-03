8fc220ab5c8a4fd51e13abfbe0f4cdc688a3538a
Pastafredda <justspotify09@gmail.com>
2023-06-03 13:14:14 +0200

    fine prima parte di esercizio

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -136,12 +136,28 @@ export default {
                     </div>
 
                 </div>
-                <div class="logo-footer">
-                    <img src="../assets/img/dc-logo-bg.png" alt="">
+                <div class="logo-footer"></div>
+            </div>
+        </div>
+        <div class="finale">
+            <div class="container">
+                <div class="nav-footer">
+                    <div class="sign-up">
+                        <a href="#">SIGN-UP NOW!</a>
+                    </div>
+                    <div>
+                        <ul class="social">
+                            <li>FOLLOW US</li>
+                            <li><a href="#"><img src="../assets/img/footer-facebook.png" alt=""></a></li>
+                            <li><a href="#"><img src="../assets/img/footer-twitter.png" alt=""></a></li>
+                            <li><a href="#"><img src="../assets/img/footer-youtube.png" alt=""></a></li>
+                            <li><a href="#"><img src="../assets/img/footer-pinterest.png" alt=""></a></li>
+                            <li><a href="#"><img src="../assets/img/footer-periscope.png" alt=""></a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-
     </footer>
 </template>
 
@@ -153,6 +169,7 @@ footer {
     background-repeat: no-repeat;
     background-size: cover;
     height: 300px;
+    position: relative;
 
     .container-footer {
         display: flex;
@@ -160,23 +177,72 @@ footer {
 
         .menu-footer {
             display: flex;
-            gap: 15px;
+            gap: 20px;
             padding: 30px;
 
 
             h5 {
                 color: white;
+                margin-bottom: 10px;
 
             }
 
             a {
                 color: #777570;
-                font-size: 12px;
+                font-size: 10px;
             }
         }
 
         .logo-footer {
-            height: 400px;
+            background-image: url(../assets/img/dc-logo-bg.png);
+            height: 300px;
+            width: 460px;
+            background-repeat: no-repeat;
+            background-size: 100%;
+            background-position: 0 -75px;
+        }
+    }
+
+}
+
+.finale {
+    background-color: #313232;
+    width: 100%;
+    height: 100px;
+    display: flex;
+    align-items: center;
+
+    .nav-footer {
+        @include display-flex;
+
+        .sign-up {
+
+
+            a {
+                padding: 10px;
+                color: white;
+                text-align: center;
+                border: 2px solid #007be9;
+                font-size: 14px;
+
+            }
+        }
+
+        .social {
+            display: flex;
+            align-items: center;
+
+            li {
+                font-size: 16px;
+                color: #0080f8;
+                font-weight: 600;
+                margin-right: 20px;
+
+                img {
+                    width: 24px;
+                }
+
+            }
         }
     }
 
