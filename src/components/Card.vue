74d13760f440d2582f32e4c55b43d0c8e6420391
Pastafredda <justspotify09@gmail.com>
2023-06-04 21:51:26 +0200

    fine progetto

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,10 +1,27 @@
 <script>
 export default {
     name: "Card",
+    props: {
+        details: Object
+    }
 
 }
 </script>
 
-<template></template>
+<template>
+    <div class="comic">
+        <img :src="details.thumb" alt="details.series">
+        <h5>{{ details.series }}</h5>
+    </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.comic {
+    width: calc(100% / 6);
+    padding: 20px;
+}
+
+h5 {
+    color: white;
+}
+</style>
